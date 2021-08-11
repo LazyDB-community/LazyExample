c3038fb33dd9d7802588f9f6f14aaefe9cc82709
MoskalykA <45717749+MoskalykA@users.noreply.github.com>
2021-08-11 12:52:04 +0200

    ✨ Adding an example of a login (Made in Csharp)

---
+++ LoginCsharp/LoginCsharp/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LazyCsharp;
+
+namespace LoginCsharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Database db;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            db = new Database("eu.indivis.cloud", 42600, delegate (Object s) {
+                Console.WriteLine("Good!");
+            }, delegate (Object s) {
+                Console.WriteLine("Is not good!");
+            });
+        }
+
+        private void connect(object sender, RoutedEventArgs e)
+        {
+            Callback callback = new Callback();
+            callback.success = delegate (Newtonsoft.Json.Linq.JToken s) {
+                MessageBox.Show("You are logged in!");
+            };
+            callback.fail = delegate (Newtonsoft.Json.Linq.JToken s) {
+                MessageBox.Show("Your information isn't good!");
+            };
+
+            db.connect(email.Text, password.Password, callback);
+        }
+    }
+}
